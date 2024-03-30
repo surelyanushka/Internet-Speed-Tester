18bb5ce50b0f3f53fc13b866dc76e0cd4d2ccbff
surelyanushka <anushka.tawte@gmail.com>
2024-03-30 15:10:45 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,70 @@
+let startTime, endTime;
+let imageSize = "";
+let image = new Image();
+
+let bitSpeed = document.getElementById("bits"),
+    kbSpeed = document.getElementById("kbs"),
+    mbSpeed = document.getElementById("mbs"),
+    info = document.getElementById("info");
+
+let totalBitSpeed = 0;
+let totalKbSpeed = 0;
+let totalMbSpeed = 0;
+let numTests = 4;
+let testCompleted = 0;
+
+let imageApi = "https://source.unsplash.com/random?topic=nature";
+
+image.onload = async function(){
+    endTime = new Date().getTime();
+
+    await fetch(imageApi).then((response) => {
+        imageSize = response.headers.get("content-length");
+        calculateSpeed()
+
+    })
+};
+
+//this calculates speed
+function calculateSpeed(){
+    let timeDuration = (endTime - startTime) / 1000;
+    // Total bits
+    let loadedBits = imageSize * 8;
+    let speedInBts = loadedBits / timeDuration;
+    let speedInKbs = speedInBts / 1024;
+    let speedInMbs = speedInKbs / 1024;
+
+    totalBitSpeed += speedInBts;
+    totalKbSpeed += speedInKbs;
+    totalMbSpeed += speedInMbs;
+
+    testCompleted++;
+
+    if (testCompleted === numTests) {
+        let averageSpeedInBps = (totalBitSpeed / numTests).toFixed(2);
+        let averageSpeedInKbps = (totalKbSpeed / numTests).toFixed(2);
+        let averageSpeedInMbps = (totalMbSpeed / numTests).toFixed(2);
+
+        // Display average speeds
+        bitSpeed.innerHTML += `${averageSpeedInBps}`;
+        kbSpeed.innerHTML += `${averageSpeedInKbps}`;
+        mbSpeed.innerHTML += `${averageSpeedInMbps}`;
+        info.innerHTML = "Test Completed!";
+    } else {
+        // Run the next test
+        startTime = new Date().getTime();
+        image.src = imageApi;
+    }
+}
+const init = async () => {
+    info.innerHTML = "Testing...";
+    startTime = new Date().getTime();
+    image.src = imageApi;
+};
+
+// Run tests when window loads
+window.onload = () => {
+    for (let i = 0; i < numTests; i++) {
+        init();
+    }
+};
